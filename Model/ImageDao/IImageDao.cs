81da6222354825e2a46344971c2204ff68a5e3bf
Daniel Silva Iglesias <daniel.silva.iglesias>
2021-10-13 16:27:45 +0200

    DAO Imagen

---
+++ Model/ImageDao/IImageDao.cs
@@ -0,0 +1,19 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.ImageDao
+{
+
+    public class ImageDaoEntityFramework :
+        GenericDaoEntityFramework<Image, Int64>, IImageDao
+    {
+        List<Image> IImageDao.FindByImageId(long imgId, int startIndex, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ Model/ImageDao/ImageDaoEntityFramework.cs
@@ -0,0 +1,17 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using System;
+using System.Collections.Generic;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.ImageDao
+{
+
+    public interface IImageDao : IGenericDao<Image, Int64>
+    {
+
+        List<Image> FindByImageId(long imgId, int startIndex, int count);
+
+
+
+    }
+
+}
