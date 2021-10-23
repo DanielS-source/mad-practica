e8f0a8fc6bfe43db3476cf43a220b788a542839f
Yago Mira <yago.mira@udc.es>
2021-10-23 16:54:50 +0200

    Added 'Ver Seguidos' + 'Ver Seguidores' Daos and EntityFramework

---
+++ Model/FollowDao/FollowDaoEntityFramework.cs
@@ -0,0 +1,41 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.FollowDao
+{
+    public class FollowDaoEntityFramework :
+    GenericDaoEntityFramework<Follow, Int64>, IFollowDao
+    {
+        //Follow
+        //UserProfile
+        public List<Follow> GetFollowers(long userProfileId, int startIndex, int count)
+        {
+            DbSet<UserProfile> followers = Context.Set<UserProfile>();
+
+            List<Follow> result =
+                (from fw in followers
+                 where fw.usrId == userProfileId
+                 select fw.Follow).First().ToList<Follow>();
+
+            return result;
+        }
+
+        public List<UserProfile> GetFollows(long userProfileId, int startIndex, int count)
+        {
+            DbSet<Follow> follow = Context.Set<Follow>();
+
+            List<UserProfile> result =
+                (from fw in follow
+                 where fw.followerId == userProfileId
+                 select fw.UserProfile1).ToList<UserProfile>();
+
+            return result;
+        }
+    }
+
+}
--- Model/FollowDao/IFollowDao.cs
+++ Model/FollowDao/IFollowDao.cs
@@ -9,7 +9,34 @@ namespace Es.Udc.DotNet.PracticaMaD.Model.FollowDao
 {
     interface IFollowDao : IGenericDao<Follow, Int64>
     {
+        /// <summary>
+        /// Returns a list of followers search by userId
+        /// </summary>
+        /// <param name="userProfileId">The user identifier.</param>
+        /// <param name="startIndex">The index of the first object to return
+        /// (starting in 0).</param>
+        /// <param name="count">The maximum number of objects to return.
+        /// </param>
+        /// <returns>
+        /// The collection of <c>Follow</c> objects.
+        /// </returns>
 
+        /*SQL: Tomar todos los valores en Follow donde usrId=idUser && Tomamos followerId*/
+        List<Follow>GetFollowers(long userProfileId, int startIndex, int count);
+
+        /// <summary>
+        /// Returns a list of all users who userId follow
+        /// </summary>
+        /// <param name="userProfileId">The user identifier.</param>
+        /// <param name="startIndex">The index of the first object to return
+        /// (starting in 0).</param>
+        /// <param name="count">The maximum number of objects to return.
+        /// </param>
+        /// <returns>
+        /// The collection of <c>Follow</c> objects.
+        /// </returns>
+        /*SQL: Tomar todos los valores en Follow donde followerId=idUser && Tomamos usrId*/
+        List<UserProfile> GetFollows(long userProfileId, int startIndex, int count);
 
 
     }
