d3426e2773165fa7a70f198df65c4d4561fc2ed9
Daniel Silva Iglesias <daniel.silva.iglesias>
2021-10-27 16:56:28 +0200

    Model - Fix ImageRelatedService

---
+++ Model/ImageRelatedService/IImageRelatedService.cs
@@ -0,0 +1,22 @@
+﻿using Es.Udc.DotNet.PracticaMaD.Model.CommentsDao;
+using Es.Udc.DotNet.PracticaMaD.Model.ImageDao;
+using Es.Udc.DotNet.PracticaMaD.Model.LikeDao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.ImageRelatedService
+{
+    interface IImageRelatedService
+    {
+
+        IImageDao ImageDao { set; }
+
+        ICommentsDao CommentsDao { set; }
+
+        ILikeDao LikeDao { set; }
+
+    }
+}
---
+++ Model/ImageRelatedService/ImageRelatedService.cs
@@ -0,0 +1,30 @@
+﻿using Es.Udc.DotNet.PracticaMaD.Model.CommentsDao;
+using Es.Udc.DotNet.PracticaMaD.Model.ImageDao;
+using Es.Udc.DotNet.PracticaMaD.Model.LikeDao;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.ImageRelatedService
+{
+    public class ImageRelatedService : IImageRelatedService
+    {
+        public ImageRelatedService()
+        {
+        }
+
+        [Inject]
+        public IImageDao ImageDao { private get; set; }
+
+        [Inject]
+        public ICommentsDao CommentsDao { private get; set; }
+
+        [Inject]
+        public ILikeDao LikeDao { private get; set; }
+
+
+    }
+}
