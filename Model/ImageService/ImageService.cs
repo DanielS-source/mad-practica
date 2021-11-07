68e778ffffc7c2c1fcc220bcca8e0916a5bfd4a4
Daniel Silva Iglesias <daniel.silva.iglesias>
2021-11-07 17:30:29 +0100

    Model - Change of implementation of cache

--- Model/ImageService/ImageService.cs
+++ Model/ImageService/ImageService.cs
@@ -8,12 +8,12 @@ using Es.Udc.DotNet.ModelUtil.Exceptions;
 using Es.Udc.DotNet.ModelUtil.Transactions;
 using Ninject;
 using System.Runtime.Caching;
+using Es.Udc.DotNet.PracticaMaD.Model.Cache;
 
 namespace Es.Udc.DotNet.PracticaMaD.Model.ImageService
 {
     public class ImageService : IImageService
     {
-        private MemoryCache ImageCache = new MemoryCache("MemoryCache");
         private List<string> keys = new List<string>();
 
         public ImageService()
@@ -33,24 +33,18 @@ namespace Es.Udc.DotNet.PracticaMaD.Model.ImageService
         [Transactional]
         public ImageBlock SearchImages(string keywords, string category, int startIndex, int count)
         {
+            List<Image> images = new List<Image>();
+
             //Checks if its in the cache
-            List<Image> images = (List<Image>)ImageCache[keywords + category];
-
-            //If its not found in the cache
-            if (images == null) {
-
+            if (ImageCache.Exists(keywords + category)) {
+                images = ImageCache.Get<List<Image>>(keywords + category);
+            }
+            else
+            {
                 images = ImageDao.FindByKeywordsAndCategory(keywords, category, startIndex, count);
-                keys.Add(keywords + category);
 
-                if (keys.Count > 5)
-                {
-                    //Removes last item if cache size exceeds 5
-                    ImageCache.Remove(keys.First());
-                    keys.Remove(keys.First());
-                }
-                //Adds entry to cache
-                ImageCache[keywords + category] = images;
-
+                //Adds the results to the cache
+                ImageCache.Add(keywords + category, images);
             }
 
             bool existMoreImages = (images.Count == count);
