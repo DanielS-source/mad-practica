7a3d53df37f904cd8f63fd4a8fce9efc612155f7
Daniel Silva Iglesias <daniel.silva.iglesias>
2021-12-19 18:23:57 +0100

    IoC

--- Web/HTTP/Util/IoC/IoCManagerNinject.cs
+++ Web/HTTP/Util/IoC/IoCManagerNinject.cs
@@ -1,8 +1,11 @@
-﻿using Castle.MicroKernel;
-using Es.Udc.DotNet.MiniBank.Model.AccountDao;
-using Es.Udc.DotNet.MiniBank.Model.AccountOperationDao;
-using Es.Udc.DotNet.MiniBank.Model.AccountService;
-using Es.Udc.DotNet.ModelUtil.IoC;
+﻿using Es.Udc.DotNet.ModelUtil.IoC;
+using Es.Udc.DotNet.PracticaMaD.Model.CategoryDao;
+using Es.Udc.DotNet.PracticaMaD.Model.CommentsDao;
+using Es.Udc.DotNet.PracticaMaD.Model.ImageDao;
+using Es.Udc.DotNet.PracticaMaD.Model.ImageService;
+using Es.Udc.DotNet.PracticaMaD.Model.TagDao;
+using Es.Udc.DotNet.PracticaMaD.Model.UserProfileDao;
+using Es.Udc.DotNet.PracticaMaD.Model.UserService;
 using Ninject;
 using System.Configuration;
 using System.Data.Entity;
@@ -19,16 +22,26 @@ namespace Es.Udc.DotNet.MiniBank.HTTP.Util.IoC
             settings = new NinjectSettings() { LoadExtensions = true };
             kernel = new StandardKernel(settings);
 
-            /*** AccountService ***/
-            kernel.Bind<IAccountService>().To<AccountService>();
+            /*** UserService ***/
+            kernel.Bind<IUserService>().To<UserService>();
 
-            /*** AccountDao ***/
-            kernel.Bind<IAccountDao>().
-                To<AccountDaoEntityFramework>();
+            /*** ImageService ***/
+            kernel.Bind<IImageService>().To<ImageService>();
 
-            /*** AccountDaoEntityFramework ***/
-            kernel.Bind<IAccountOperationDao>().
-                To<AccountOperationDaoEntityFramework>();
+            /*** UserProfileDao ***/
+            kernel.Bind<IUserProfileDao>().To<UserProfileDaoEntityFramework>();
+
+            /*** ImageDao ***/
+            kernel.Bind<IImageDao>().To<ImageDaoEntityFramework>();
+
+            /*** CommentsDao ***/
+            kernel.Bind<ICommentsDao>().To<CommentsDaoEntityFramework>();
+
+            /*** TagDao ***/
+            kernel.Bind<ITagDao>().To<TagDaoEntityFramework>();
+
+            /*** CategoryDao ***/
+            kernel.Bind<ICategoryDao>().To<CategoryDaoEntityFramework>();
 
             /*** DbContext ***/
             string connectionString =
