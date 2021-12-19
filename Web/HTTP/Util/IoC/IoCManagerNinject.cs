7e991ca3f2e9de4099a94a2ae7bf7a55f9b553cb
Daniel Silva Iglesias <daniel.silva.iglesias>
2021-12-19 17:49:18 +0100

    Http

---
+++ Web/HTTP/Util/IoC/IoCManagerNinject.cs
@@ -0,0 +1,48 @@
+﻿using Castle.MicroKernel;
+using Es.Udc.DotNet.MiniBank.Model.AccountDao;
+using Es.Udc.DotNet.MiniBank.Model.AccountOperationDao;
+using Es.Udc.DotNet.MiniBank.Model.AccountService;
+using Es.Udc.DotNet.ModelUtil.IoC;
+using Ninject;
+using System.Configuration;
+using System.Data.Entity;
+
+namespace Es.Udc.DotNet.MiniBank.HTTP.Util.IoC
+{
+    internal class IoCManagerNinject : IIoCManager
+    {
+        private static IKernel kernel;
+        private static NinjectSettings settings;
+
+        public void Configure()
+        {
+            settings = new NinjectSettings() { LoadExtensions = true };
+            kernel = new StandardKernel(settings);
+
+            /*** AccountService ***/
+            kernel.Bind<IAccountService>().To<AccountService>();
+
+            /*** AccountDao ***/
+            kernel.Bind<IAccountDao>().
+                To<AccountDaoEntityFramework>();
+
+            /*** AccountDaoEntityFramework ***/
+            kernel.Bind<IAccountOperationDao>().
+                To<AccountOperationDaoEntityFramework>();
+
+            /*** DbContext ***/
+            string connectionString =
+                ConfigurationManager.ConnectionStrings["MiniBankEntities"].ConnectionString;
+
+            kernel.Bind<DbContext>().
+                    ToSelf().
+                    InSingletonScope().
+                    WithConstructorArgument("nameOrConnectionString", connectionString);
+        }
+
+        public T Resolve<T>()
+        {
+            return kernel.Get<T>();
+        }
+    }
+}
